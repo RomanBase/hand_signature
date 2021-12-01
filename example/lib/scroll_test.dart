a70cb5d0a410644b07bbba76827f8ffce1d9ee64
RomanBase <ro.hornak@gmail.com>
2021-12-01 13:18:20 +0100

    minor test update

--- example/lib/scroll_test.dart
+++ example/lib/scroll_test.dart
@@ -35,7 +35,7 @@ class _ScrollTestState extends State<ScrollTest> {
               type: SignatureDrawType.shape,
               onPointerDown: () {
                 setState(() {
-                  scrollEnabled = false;
+                  scrollEnabled = true;
                 });
               },
               onPointerUp: () {
